dc6d6419d4c19952ed0fabdcc5a1a24f5b136ae4
RSangaradoss <90277107+RSangaradoss@users.noreply.github.com>
2022-01-19 22:26:27 +0530

    Day-3 Assignment submitted

---
+++ LetsupGDay3ASS.py
@@ -0,0 +1,9 @@
+name = input(f"Enter Your Name : ")
+age = int(input(f"Enter Your Age : "))
+marks = float(input(f"Enter Your Marks : "))
+address = input(f"Enter Your Address : ")
+time = eval(input(f"Enter Time : "))
+print()
+
+print("\t"'T','H','A','N','K',' ','YOU', sep='')
+print("\t\t"'By ','r.sangaradoss', sep='@',end=':)')
